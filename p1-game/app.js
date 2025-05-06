49bd97a5aa79cda524a29ff37b04b3fa15a2732a
Abu_Sufiun <107828483+sufiun27@users.noreply.github.com>
2025-05-06 12:07:28 +0600

    06-May-25

--- p1-game/app.js
+++ p1-game/app.js
@@ -3,28 +3,96 @@ function getRandomValue(min, max) {
 }
 
 Vue.createApp({
+
     data() {
         return {
             monsterHealt: 100,
             playerHealt: 100,
+            counter: 0,
+            batteleLog: [],
+            isGameRunning: true,
+            whoWinMassage: '',
         }
     },
-    methods: {
-        attackMosnter(){
+    computed: {
+        monsterHealthBar() {
+            if (this.monsterHealt < 0) {
+              this.monsterHealt = 0;
+              return { width: this.monsterHealt + '%' };
+            }else{
+                return { width: this.monsterHealt + '%' };
+            }
             
+        },
+        playerHealthBar() {
+            if (this.playerHealt < 0) {
+             this.playerHealt = 0;
+             return { width: this.playerHealt + '%' };
+            }else{
+                return { width: this.playerHealt + '%' };
+            }
+        },
+    },
+    watch: {
+        monsterHealt(value) {
+            if (value == 0 && this.playerHealt == 0) {
+                this.isGameRunning = false;
+                this.whoWinMassage = 'Draw!';
+            }
+            else if (value > 100) {
+                this.monsterHealt = 100;
+                this.isGameRunning = false;
+                this.whoWinMassage = 'Monster won!';
+            }
+            else if (value <= 0) {
+                this.isGameRunning = false;
+                this.whoWinMassage = 'Player won!';
+            }
+        },
+        playerHealt(value) {
+            if (value == 0 && this.monsterHealt == 0) {
+                this.isGameRunning = false;
+                this.whoWinMassage = 'Draw!';
+            }
+            else if (value > 100) {
+                this.playerHealt = 100;
+                this.isGameRunning = false;
+                this.whoWinMassage = 'Monster won!';
+            }
+            else if (value <= 0) {
+                this.isGameRunning = false;
+                this.whoWinMassage = 'Monster won!';
+            }
+        },
+    },
+    methods: {
+        setBattleLog(who, action, value) {
+            this.batteleLog.unshift({
+                who: who,
+                action: action,
+                value: value,
+            });
+        },
+
+        attackMosnter(){
+            this.counter++;
             const attackValue = getRandomValue(5, 10);
             this.monsterHealt -= attackValue;
             this.attackPlayer();
+            this.setBattleLog('Player', 'attacked', attackValue);
             console.log('attackMonster: ' , this.monsterHealt);
         },
         attackPlayer(){
-            const attackValue = getRandomValue(8, 15);
+            const attackValue = getRandomValue(7, 12);
             this.playerHealt -= attackValue;
+            this.setBattleLog('Monster', 'attacked', attackValue);
         },
         specialAttackMonster(){
+            this.counter++;
             const attackValue = getRandomValue(10, 20);
             this.monsterHealt -= attackValue;
             this.attackPlayer();
+            this.setBattleLog('Player', 'special attacked', attackValue);
             console.log('specialAttackMonster: ' , this.monsterHealt);
         },
         specialAttackPlayer(){},
@@ -33,6 +101,7 @@ Vue.createApp({
             this.playerHealt += healValue;
             this.attackPlayer();
             console.log('healPlayer: ' , this.playerHealt);
+            this.setBattleLog('Player', 'healed', healValue);
         },
         healMonster(){},
         surrender(){
@@ -40,6 +109,12 @@ Vue.createApp({
             this.monsterHealt = 100;
             console.log('surrender: ' , this.playerHealt, this.monsterHealt);
         },
-        resetGame(){},
+        resetGame(){
+            this.playerHealt = 100;
+            this.monsterHealt = 100;
+            this.batteleLog = [];
+            this.isGameRunning = true;
+            console.log('resetGame: ' , this.playerHealt, this.monsterHealt);
+        },
     },
 }).mount('#game');
--- v2/app.js
+++ v2/app.js
@@ -1,40 +1,46 @@
 const app = Vue.createApp({
-    data() {
-      return {
-        counter: 0,
-        name: '',
-        completeName: '',
-        nameError: '',
-        details: '',
-        submittedDetails: '',
-      };
+  data() {
+    return {
+      counter: 0,
+      name: '',
+      lastName: '',
+      nameError: '',
+      details: '',
+      submittedDetails: ''
+    };
+  },
+
+  computed: {
+    getfullName() {
+      if (this.name && this.lastName) {
+        return this.name + ' ' + this.lastName;
+      }
+      return this.name || this.lastName || 'No name provided';
+    }
+  },
+
+  methods: {
+    increment() {
+      this.counter++;
     },
-    methods: {
-      increment() {
-        this.counter++;
-      },
-      decrement() {
-        this.counter--;
-      },
-      setName(event) {
-        this.name = event.target.value;
-        this.nameError = this.name.length > 20 ? 'Name must be 20 characters or less' : '';
-      },
-      setCompleteName() {
-        if (!this.name) {
-          this.nameError = 'Please enter a name';
-          return;
-        }
-        this.completeName = this.name;
-        this.name = '';
-        this.nameError = '';
-      },
-      handleSubmit() {
-        if (!this.details) return;
-        this.submittedDetails = this.details;
-        this.details = '';
-      },
+    decrement() {
+      this.counter--;
     },
-  });
-  
-  app.mount('#app');
+    setFullName() {
+      if (!this.name || !this.lastName) {
+        this.nameError = 'Please enter both a first name and a last name';
+        return;
+      }
+      this.nameError = '';
+      this.name = '';
+      this.lastName = '';
+    },
+    handleSubmit() {
+      if (!this.details) return;
+      this.submittedDetails = this.details;
+      this.details = '';
+    }
+  }
+});
+
+app.mount('#app');
